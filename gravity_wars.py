c74fedfa20854db6ee7277ceb5e75e634794bd72
gentry <gma23@txstate.edu>
2021-10-13 13:36:34 -0500

    added level label

--- gravity_wars.py
+++ gravity_wars.py
@@ -47,9 +47,12 @@ if __name__ == '__main__':
     #Setup and Initialize
     print('Launching Gravity Wars')
     pygame.init()
+    pygame.display.set_caption('Gravity Wars')
     SCREEN = pygame.display.set_mode((GW_globals.WIDTH,GW_globals.HEIGHT))
     running = True
     main_font = pygame.font.SysFont('Courier 10', 50)
+
+    level = 1
 
     #Load Images
     PLAYER_SHIP = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship.png')), (20, 20))
@@ -57,7 +60,7 @@ if __name__ == '__main__':
     BG = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'background.png')), (GW_globals.WIDTH, GW_globals.HEIGHT))
     PLANET = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'planet.png')), (GW_globals.PLANET_SIZE, GW_globals.PLANET_SIZE))
     ROCK = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'rock.png')), (20, 20))
-    game_label = main_font.render('Gravity Wars', 1, (255, 255, 255))
+    game_label = main_font.render(f'Level {level}', 1, (255, 255, 255))
     static_images = [
         [BG, (0, 0)],
         [game_label, (GW_globals.WIDTH//2 - 100, GW_globals.HEIGHT//40)],
@@ -66,9 +69,7 @@ if __name__ == '__main__':
     player_ship = Player(GW_globals.WIDTH//4, GW_globals.WIDTH//4, 100, -100, PLAYER_SHIP)
     planet = Planet(PLANET)
     enemies = [
-        Rock(600, 400,40, 200, ROCK, 0),
-        Rock(550, 400,40, 200, ROCK, 0),
-        Rock(500, 400,40, 200, ROCK, 0)
+        Rock(600, random.randint(300, 500) ,40, 200, ROCK, 0)
     ]
     projectiles = []
 
@@ -85,6 +86,18 @@ if __name__ == '__main__':
         player_ship.move(keys, projectiles, dt)
         projectiles = [p for p in projectiles if not p.dead]
         enemies = [e for e in enemies if not e.dead]
+        if enemies == []:
+            enemies = [
+                Rock(600, random.randint(300, 500) ,40, 200, ROCK, 0),
+                Rock(550, random.randint(300, 500),40, 200, ROCK, 0),
+                Rock(500, random.randint(300, 500),40, 200, ROCK, 0)
+            ]
+            level += 1
+            game_label = main_font.render(f'Level {level}', 1, (255, 255, 255))
+            static_images = [
+                [BG, (0, 0)],
+                [game_label, (GW_globals.WIDTH//2 - 100, GW_globals.HEIGHT//40)],
+            ]
         for p in projectiles:
             p.move(dt)
         for e in enemies:
