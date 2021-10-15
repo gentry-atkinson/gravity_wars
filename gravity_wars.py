a2cc8aff04228a832bbf4e8ac3ea63f99d4776b2
gentry <gma23@txstate.edu>
2021-10-15 09:23:34 -0500

    adding score counter

--- gravity_wars.py
+++ gravity_wars.py
@@ -39,9 +39,11 @@ if __name__ == '__main__':
 
     #Load Images
     game_label = main_font.render(f'Level {level}', 1, (255, 255, 255))
+    score_label = main_font.render('Score: 0', 1, (255, 255, 255))
     static_images = [
         [IL.BG, (0, 0)],
         [game_label, (GW_globals.WIDTH//2 - 100, GW_globals.HEIGHT//40)],
+        [score_label,(GW_globals.WIDTH//2 - 100, GW_globals.HEIGHT - 100)]
     ]
 
     player_ship = Player(GW_globals.WIDTH//4, GW_globals.WIDTH//4, 100, -100, IL.PLAYER_SHIP)
