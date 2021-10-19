dbe35a5f7df5ee9285c4c6de1bca00fb1e4b4968
Gentry Atkinson <gma23@txstate.edu>
2021-10-19 11:54:00 -0500

    making ship icon a dict

--- Image_Loader.py
+++ Image_Loader.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import GW_globals
 
-PLAYER_SHIP = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship.png')), (30, 40))
+#PLAYER_SHIP = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship.png')), (30, 40))
 OTHER_SHIP = pygame.image.load(os.path.join('assets/imgs', 'crescent.png'))
 BG = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'background.png')), (GW_globals.WIDTH, GW_globals.HEIGHT))
 PLANET = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'planet.png')), (GW_globals.PLANET_SIZE, GW_globals.PLANET_SIZE))
@@ -10,5 +10,24 @@ ROCK = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'roc
 SAT = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'sat.png')), (30, 30))
 LAS = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'shot.png')), (10, 10))
 ALERT = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'alert.png')), (40, 40))
-PLAYER_SHIP_BURN = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_burning.png')), (30, 40))
+#PLAYER_SHIP_BURN = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_burning.png')), (30, 40))
 SHIELD = pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'shield.png')), (46, 58))
+
+PLAYER_SHIP = {
+    "no shield no burn": = [
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_no_shield_1.png')), (50, 50)),
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_no_shield_1.png')), (50, 50))
+    ],
+    "with shield no burn": [
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_with_shield_1.png')), (50, 50)),
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_with_shield_2.png')), (50, 50))
+    ],
+    "no shield with burn": [
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_no_shield_burning_1.png')), (50, 50)),
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_no_shield_burning_2.png')), (50, 50))
+    ],
+    "with shield with burn": [
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_with_shield_burning_1.png')), (50, 50)),
+        pygame.transform.scale(pygame.image.load(os.path.join('assets/imgs', 'player_ship_with_shield_burning_2.png')), (50, 50))
+    ]
+}
